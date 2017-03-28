cb8c390042129f52bbde34562101255954173075
BergerAl <berger@imesbv02.imes.intern>
2017-03-28 15:39:05 +0200

    changed to categorical

--- train_CNN.py
+++ train_CNN.py
@@ -5,49 +5,67 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras import optimizers
-
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 img_width, img_height = 150, 150
 
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
 
+classes_amount = 5
+
 # used to rescale the pixel values from [0, 255] to [0, 1] interval
-datagen = ImageDataGenerator(rescale=1./255)
+datagen = ImageDataGenerator(
+        rescale=1./255,        # normalize pixel values to [0,1]
+        shear_range=0.2,       # randomly applies shearing transformation
+        zoom_range=0.2,        # randomly applies shearing transformation
+        horizontal_flip=True)  # randomly flip the images
+
+# img = load_img('data/train/class1/img_1.jpg')
+# x = img_to_array(img)
+# x = x.reshape((1,) + x.shape)
+# i = 0
+# for batch in datagen.flow(x, batch_size=1,
+#                           save_to_dir='preview', save_prefix='class1', save_format='jpeg'):
+#     i += 1
+#     if i > 20:
+#         break  # otherwise the generator would loop indefinitely
 
 # automagically retrieve images and their classes for train and validation sets
 train_generator = datagen.flow_from_directory(
         train_data_dir,
         target_size=(img_width, img_height),
         batch_size=16,
-        class_mode='binary')
+        class_mode='categorical')
 
 validation_generator = datagen.flow_from_directory(
         validation_data_dir,
         target_size=(img_width, img_height),
         batch_size=32,
-        class_mode='binary')
+        class_mode='categorical')
 
 #Model
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(img_width, img_height, 3)))
+model.add(Conv2D(64, (3, 3), input_shape=(img_width, img_height, 3)))   #32
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (3, 3)))
+model.add(Conv2D(64, (3, 3)))       #32
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(128, (3, 3)))      #64
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(128))               #64
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Dense(classes_amount, activation='softmax'))
+#model.add(Dense(1))
+#model.add(Activation('sigmoid'))    #only for binary classes
+
 
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
@@ -55,14 +73,14 @@ model.compile(loss='binary_crossentropy',
 
 batch_size = 16
 nb_epoch = 30
-nb_train_samples = 214*4
-nb_validation_samples = 27*4
+nb_train_samples = 924#96*5                # old_data == 283,data ==214
+nb_validation_samples = 360#48*5            # old_data == 38,data ==27
 
 model.fit_generator(
         train_generator,
-        steps_per_epoch=nb_train_samples/batch_size,
+        steps_per_epoch=nb_train_samples / batch_size,
         epochs=nb_epoch,
         validation_data=validation_generator,
-        validation_steps=nb_validation_samples/batch_size)
+        validation_steps=nb_validation_samples / batch_size)
 
-model.save_weights('models/basic_cnn_20_epochs.h5')
+model.save_weights('models/basic_cnn_30_epochs.h5')
