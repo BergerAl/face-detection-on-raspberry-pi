1f8a1903bb0d4ed5caca98bbc40f66b1f9a8ce54
BergerAl <aleksej.berger@t-online.de>
2017-03-30 21:14:58 +0200

    added Apache 2.0 license

--- train_CNN.py
+++ train_CNN.py
@@ -1,3 +1,20 @@
+#Copyright [2017] [Alexej Berger <alexej.berger@t-online.de>]
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+#The above copyright notice and this permission notice shall be
+#included in all copies or substantial portions of the Software.
+
 import os
 import numpy as np
 from keras.models import Sequential
@@ -46,8 +63,11 @@ validation_generator = datagen.flow_from_directory(
 
 #Model
 model = Sequential()
+# Convlutional Layer, 32 filters with a 3x3 Convlutional
 model.add(Conv2D(32, (3, 3), input_shape=(img_width, img_height, 3)))   #32
+#relu for nonlinearities
 model.add(Activation('relu'))
+#Reducing the complexity of feature maps. 2x2 filter
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(32, (3, 3)))       #32
@@ -58,7 +78,10 @@ model.add(Conv2D(64, (3, 3)))      #64
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+#preventing overfitting
+#Flatten into 1D
 model.add(Flatten())
+#Fully connected layer
 model.add(Dense(64))               #64
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
